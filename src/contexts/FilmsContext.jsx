dc274b9a9cbba4021113b13c5b04eff9a3c595b4
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-28 11:36:37 +0100

    create contexts folder and create FilmsContexts in it

---
+++ src/contexts/FilmsContext.jsx
@@ -0,0 +1,11 @@
+/*Milestone 0:
+Progettare la struttura del global state sulla linea degli esercizi svolti nei giorni
+precedenti. */
+
+import { createContext } from "react";
+
+//creo la costante FilmsContext
+const FilmsContext = createContext()
+
+//esporto FilmsContext
+export default FilmsContext
